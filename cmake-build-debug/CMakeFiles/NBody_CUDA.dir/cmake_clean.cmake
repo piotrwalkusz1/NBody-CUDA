ab57a1b376810b2f0f737c87ad3061d601d08dd2
Piotr Walkusz <piotrwalkusz1@gmail.com>
2021-01-23 16:21:41 +0100

    Initial

---
+++ cmake-build-debug/CMakeFiles/NBody_CUDA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NBody_CUDA.dir/cmake_device_link.o"
+  "CMakeFiles/NBody_CUDA.dir/main.cu.o"
+  "NBody_CUDA"
+  "NBody_CUDA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/NBody_CUDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NBody_CUDA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NBody_CUDA.
+# This may be replaced when dependencies are built.
